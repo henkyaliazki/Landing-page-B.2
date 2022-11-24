efd3975e856a1c1581abe25898b99fc6512bb5de
henkyaliazki <henkyaliazkipratama>
2022-11-24 17:29:36 +0700

    penambahan efect paralax pada pricing

--- js/jQuery.js
+++ js/jQuery.js
@@ -4,6 +4,22 @@ $(document).ready(function () {
   $(".scroll").on("click", function (e) {
     let alamat = $(this).attr("href");
     let addressTo = $(alamat);
-    $("html,body").animate({scrollTop: addressTo.});
+    console.log(addressTo.offset().top);
+    //   $("html,body").animate({scrollTop: addressTo.});
   });
+});
+
+// buat efect untuk pricing atau paralax
+$(window).scroll(function () {
+  let wScroll = $(this).scrollTop();
+
+  if (wScroll > $("#pricing").offset().top - 200) {
+    // ambil semua element
+    $("#pricing .fade-in").each(function (i) {
+      setTimeout(function () {
+        $("#pricing .fade-in").eq(i).addClass("munculKiri");
+      }, 400 * (i + 1));
+      return 0;
+    });
+  }
 });
